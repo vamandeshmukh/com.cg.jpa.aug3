3cbbc8e7dd4ec5bd62f36a8b34ad7923e5580021
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-01 16:00:27 +0530

    Commit

--- src/main/java/com/cg/jpa/aug3/Student.java
+++ src/main/java/com/cg/jpa/aug3/Student.java
@@ -8,11 +8,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "students")
+@Entity // mandatory 
+@Table(name = "students") // optional 
+
 public class Student implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int studentId;
--- src/main/java/com/cg/jpa/aug3/StudentTest.java
+++ src/main/java/com/cg/jpa/aug3/StudentTest.java
@@ -8,17 +8,23 @@ public class StudentTest {
 
 	public static void main(String[] args) {
 
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
-		EntityManager em = factory.createEntityManager();
-		em.getTransaction().begin();
-		Student student = new Student();
-		student.setName("John");
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("abcd");
 
-		em.persist(student);
+		EntityManager em = factory.createEntityManager();
+
+		em.getTransaction().begin();
+
+		Student student = new Student();
+		student.setName("Sonu");
+
+		em.persist(student); // insert a new record 
+
 		em.getTransaction().commit();
 
 		System.out.println("Added one student to database.");
+
 		em.close();
 		factory.close();
+
 	}
 }
