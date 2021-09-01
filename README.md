2b3e45c43a69503a749ef8e63e90a27cd5379b2c
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-01 10:52:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# com.cg.jpa.aug3
+ JPA demo project for CG trainees August 3, 2021
---
+++ src/main/java/com/cg/jpa/aug3/App.java
@@ -0,0 +1,13 @@
+package com.cg.jpa.aug3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
